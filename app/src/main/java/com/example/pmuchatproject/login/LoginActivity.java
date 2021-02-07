750ece90f8c5892212916f7e9e32c8d93c6f1de3
vivostanoev <vivostanoev@gmail.com>
2021-02-07 22:37:39 +0200

    login screen
    Signed-off-by: vivostanoev <vivostanoev@gmail.com>

---
+++ app/src/main/java/com/example/pmuchatproject/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.pmuchatproject.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pmuchatproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
