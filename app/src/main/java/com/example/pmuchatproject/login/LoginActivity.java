0155f44122919b6cc57f777b01c5993c118edda1
vivostanoev <vivostanoev@gmail.com>
2021-02-08 01:22:40 +0200

    add Sign Up Screen
    Signed-off-by: vivostanoev <vivostanoev@gmail.com>

--- app/src/main/java/com/example/pmuchatproject/login/LoginActivity.java
+++ app/src/main/java/com/example/pmuchatproject/login/LoginActivity.java
@@ -1,16 +1,67 @@
 package com.example.pmuchatproject.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.pmuchatproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private TextInputEditText etEmail, etPassword;
+    private String email, password;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+    }
+
+
+    public void btnLoginClink(View v) {
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        if (email.equals(""))
+        {
+            etEmail.setError(getString(R.string.enter_email));
+        }
+        else if(password.equals(""))
+        {
+            etPassword.setError(getString(R.string.enter_password));
+        }
+        else
+        {
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful())
+                    {
+                        //later
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Failed :" +
+                                task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
