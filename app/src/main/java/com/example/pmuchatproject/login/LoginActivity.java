f7e180a6695016443c94505a7227c916ee6c0b21
vivostanoev <vivostanoev@gmail.com>
2021-02-08 03:16:20 +0200

    add fireBase
    Signed-off-by: vivostanoev <vivostanoev@gmail.com>

--- app/src/main/java/com/example/pmuchatproject/login/LoginActivity.java
+++ app/src/main/java/com/example/pmuchatproject/login/LoginActivity.java
@@ -3,11 +3,13 @@ package com.example.pmuchatproject.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.pmuchatproject.R;
+import com.example.pmuchatproject.signup.SignupActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
@@ -30,6 +32,12 @@ public class LoginActivity extends AppCompatActivity {
 
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
+    }
+
+
+    public void tvSignupClick(View v)
+    {
+        startActivity(new Intent(this, SignupActivity.class));
     }
 
 
