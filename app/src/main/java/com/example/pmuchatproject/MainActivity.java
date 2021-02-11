c1063a038a0980a264a5f32b5de76a93babf20a1
vivostanoev <vivostanoev@gmail.com>
2021-02-12 01:41:21 +0200

    add fragments to main screen
    Signed-off-by: vivostanoev <vivostanoev@gmail.com>

--- app/src/main/java/com/example/pmuchatproject/MainActivity.java
+++ app/src/main/java/com/example/pmuchatproject/MainActivity.java
@@ -2,21 +2,103 @@ package com.example.pmuchatproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.example.pmuchatproject.profile.ProfileActivity;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabMain);
+        viewPager = findViewById(R.id.vpMain);
+
+        setViewPager();
     }
+
+
+    class Adapter extends FragmentPagerAdapter{
+
+        public Adapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position)
+            {
+                case 0:
+                    ChatFragment chatFragment = new ChatFragment();
+                    return chatFragment;
+
+                case 1:
+                    RequestFragment requestFragment = new RequestFragment();
+                    return  requestFragment;
+                case 2:
+                    FindFriendsFragment findFriendsFragment = new FindFriendsFragment();
+                    return findFriendsFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return tabLayout.getTabCount();
+        }
+    }
+
+    private void setViewPager(){
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_chat));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_requests));
+        tabLayout.addTab(tabLayout.newTab().setCustomView(R.layout.tab_findfriends));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        Adapter adapter = new Adapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        viewPager.setAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +117,36 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean doubleBackPress = false;
+
+    @Override
+    public void onBackPressed() {
+
+        if (tabLayout.getSelectedTabPosition()>0)
+        {
+            tabLayout.selectTab(tabLayout.getTabAt(0));
+        }
+        else
+        {
+            if (doubleBackPress)
+            {
+                finishAffinity();
+            }
+            else {
+                doubleBackPress= true;
+                Toast.makeText(this, "press back to exist", Toast.LENGTH_SHORT).show();
+                android.os.Handler handler = new android.os.Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        doubleBackPress = false;
+                    }
+                }, 2000);
+            }
+        }
+
+
     }
 }
